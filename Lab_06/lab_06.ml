e31fbb3452faf1d84be2c89126aff3a05aab161a
Kris Swann <swann013@umn.edu>
2017-02-21 11:00:28 -0600

    Add functions for part a of lab 6

--- Lab_06/lab_06.ml
+++ Lab_06/lab_06.ml
@@ -1,3 +1,27 @@
 
 type 'a tree = Leaf of 'a
              | Fork of 'a * 'a tree * 'a tree
+
+
+let rec t_size t =
+    match t with
+    | Leaf  _ -> 1
+    | Fork (_,s1,s2) -> 1 + t_size s1 + t_size s2
+
+
+let rec t_sum (t:int tree) =
+    match t with
+    | Leaf v -> v
+    | Fork (v,s1,s2) -> v + t_sum s1 + t_sum s2
+
+
+let rec t_charcount (t:string tree) =
+    match t with
+    | Leaf v -> String.length v
+    | Fork (v,s1,s2) -> String.length v + t_charcount s1 + t_charcount s2
+
+
+let rec t_concat (t:string tree) =
+    match t with
+    | Leaf v -> v
+    | Fork (v,s1,s2) -> v ^ t_concat s1 ^ t_concat s2
--- Lab_06/tests.ml
+++ Lab_06/tests.ml
@@ -1,4 +1,22 @@
+#use "lab_06.ml"
+
 
 let t1 = Leaf 5
 let t2 = Fork (3, Leaf 3, Fork (2, t1, t1))
 let t3 = Fork ("Hello", Leaf "World", Leaf "!")
+let t4 = Fork (7, Fork (5, Leaf 1, Leaf 2), Fork (6, Leaf 3, Leaf 4))
+
+
+
+let () =
+    assert (t_size t1 = 1);
+    assert (t_size t2 = 5);
+    assert (t_size t3 = 3);
+
+    assert (t_sum t1 = 5);
+    assert (t_sum t2 = 18);
+    assert (t_sum t4 = 28);
+
+    assert (t_charcount t3 = 11);
+
+    assert (t_concat t3 = "HelloWorld!");
