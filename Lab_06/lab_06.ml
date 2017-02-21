c013500c426527365e9ddf33b86fde48f1d32779
Kris Swann <swann013@umn.edu>
2017-02-21 11:16:15 -0600

    Add function for part b of lab 6

--- Lab_06/lab_06.ml
+++ Lab_06/lab_06.ml
@@ -25,3 +25,34 @@ let rec t_concat (t:string tree) =
     match t with
     | Leaf v -> v
     | Fork (v,s1,s2) -> v ^ t_concat s1 ^ t_concat s2
+
+
+let rec t_opt_size (t:'a option tree) =
+    match t with
+    | Leaf (None) -> 0
+    | Leaf (Some _) -> 1
+    | Fork (None,s1,s2) -> t_opt_size s1 + t_opt_size s2
+    | Fork (Some _,s1,s2) -> 1 + t_opt_size s1 + t_opt_size s2
+
+
+let rec t_opt_sum (t:int option tree) =
+    match t with
+    | Leaf (None) -> 0
+    | Leaf (Some v) -> v
+    | Fork (None,s1,s2) -> t_opt_sum s1 + t_opt_sum s2
+    | Fork (Some v,s1,s2) -> v + t_opt_sum s1 + t_opt_sum s2
+
+
+let rec t_opt_charcount (t:string option tree) =
+    match t with
+    | Leaf (None) -> 0
+    | Leaf (Some v) -> String.length v
+    | Fork (None,s1,s2) -> t_opt_charcount s1 + t_opt_charcount s2
+    | Fork (Some v,s1,s2) -> String.length v + t_opt_charcount s1 + t_opt_charcount s2
+
+let rec t_opt_concat (t:string option tree) =
+    match t with
+    | Leaf (None) -> ""
+    | Leaf (Some v) -> v
+    | Fork (None,s1,s2) -> t_opt_concat s1 ^ t_opt_concat s2
+    | Fork (Some v,s1,s2) -> v ^ t_opt_concat s1 ^ t_opt_concat s2
--- Lab_06/tests.ml
+++ Lab_06/tests.ml
@@ -5,6 +5,9 @@ let t1 = Leaf 5
 let t2 = Fork (3, Leaf 3, Fork (2, t1, t1))
 let t3 = Fork ("Hello", Leaf "World", Leaf "!")
 let t4 = Fork (7, Fork (5, Leaf 1, Leaf 2), Fork (6, Leaf 3, Leaf 4))
+let t5 : string option tree = Fork (Some "a", Leaf (Some "b"), Fork (Some "c", Leaf None, Leaf (Some "d")))
+let t7 = (Fork (Some 1, Leaf (Some 2), Fork (Some 3, Leaf None, Leaf None)))
+let t8 = (Fork (Some "a", Leaf (Some "b"), Fork (Some "c", Leaf None, Leaf (Some "d"))))
 
 
 
@@ -20,3 +23,11 @@ let () =
     assert (t_charcount t3 = 11);
 
     assert (t_concat t3 = "HelloWorld!");
+
+    assert (t_opt_size t7 = 3);
+
+    assert (t_opt_sum t7 = 6);
+
+    assert (t_opt_charcount t8 = 4);
+
+    assert (t_opt_concat t8 = "abcd");
