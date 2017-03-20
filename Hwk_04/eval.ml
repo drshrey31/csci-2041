f409fc1264d5fbb58252beb35e203c12eb799b2f
Kris Swann <swann013@umn.edu>
2017-03-20 15:02:16 -0500

    Add most of homework 4

--- Hwk_04/eval.ml
+++ Hwk_04/eval.ml
@@ -1,9 +1,9 @@
-type expr 
+type expr
   = Add of expr * expr
   | Sub of expr * expr
   | Mul of expr * expr
   | Div of expr * expr
-  
+
   | Lt of expr * expr
   | Eq of expr * expr
   | And of expr * expr
@@ -11,7 +11,7 @@ type expr
   | If of expr * expr * expr
 
   | Id of string
-  
+
   | Let of string * expr * expr
   | LetRec of string * expr * expr
 
@@ -20,36 +20,118 @@ type expr
 
   | Value of value
 
-and value 
+and value
   = Int of int
   | Bool of bool
   | Closure of string * expr * environment
+
+and environment
+  = (string * value) list
   (* You may need an extra constructor for this type. *)
 
 
 
+(* TODO: Closures. *)
+let rec freevars (e:expr) : string list = match e with
+| Add (e1,e2)
+| Sub (e1,e2)
+| Mul (e1,e2)
+| Div (e1,e2)
+| Lt (e1,e2)
+| Eq (e1,e2)
+| And (e1,e2) -> freevars e1 @ freevars e2
+| If (e1,e2,e3) -> freevars e1 @ freevars e2 @ freevars e3
+| Id s -> [s]
+| Let (s,e1,e2) -> freevars e1 @ List.filter ( (<>) s ) (freevars e2)
+| LetRec (s,e1,e2) -> List.filter ( (<>) s) (freevars e1 @ freevars e2)
+| App (e1,e2) -> List.tl (freevars e1) @ freevars e2
+| Lambda (s,e1) -> List.filter ( (<>) s) (freevars e1)
+| Value v -> match v with
+             (*
+             | Closure (s,e1,env) -> List.filter (fun x -> not (List.exists ( (=) x) (s::env))) e1
+             *)
+             | _ -> []
 
-let evaluate (e:expr) : value = 
-  raise (Failure "Complete this function...")
+
+
+(* From class. *)
+let rec lookup (n:string) (env: environment) : value =
+    match env with
+    | [] -> raise (Failure (n ^ " not in scope"))
+    | (n',v) :: rest when n = n' -> v
+    | _ :: rest -> lookup n rest
 
 
 
-(* Some sample expressions *)
+let rec eval (env: environment) (e:expr) : value =
+    match e with
+    | Value v -> v
+    | Add (e1, e2) -> (match eval env e1, eval env e2 with
+        | Int v1, Int v2 -> Int (v1 + v2)
+        | _ -> raise (Failure ("Incompatible types on Add"))
+        )
+    | Sub (e1, e2) -> (match eval env e1, eval env e2 with
+        | Int v1, Int v2 -> Int (v1 - v2)
+        | _ -> raise (Failure ("Incompatible types on Sub"))
+        )
+    | Mul (e1, e2) -> (match eval env e1, eval env e2 with
+        | Int v1, Int v2 -> Int (v1 * v2)
+        | _ -> raise (Failure ("Incompatible types on Mul"))
+        )
+    | Div (e1, e2) -> (match eval env e1, eval env e2 with
+        | Int v1, Int v2 -> Int (v1 / v2)
+        | _ -> raise (Failure ("Incompatible types on Div"))
+        )
+    | Lt (e1, e2) -> (match eval env e1, eval env e2 with
+        | Int v1, Int v2 -> Bool (v1 < v2)
+        | _ -> raise (Failure ("Incompatible types on Lt"))
+        )
+    | Eq (e1, e2) -> (match eval env e1, eval env e2 with
+        | Int v1, Int v2 -> Bool (v1 = v2)
+        | Bool v1, Bool v2 -> Bool (v1 = v2)
+        | _ -> raise (Failure ("Incompatible types on Eq"))
+        )
+    | And (e1, e2) -> (match eval env e1, eval env e2 with
+        | Bool v1, Bool v2 -> Bool (v1 && v2)
+        | _ -> raise (Failure ("Incompatible types on And"))
+        )
+    | If (e1, e2, e3) -> (match eval env e1, eval env e2, eval env e3 with
+        | Bool true, v1, _ -> v1
+        | Bool false, _, v2 -> v2
+        | _ -> raise (Failure ("Incompatible types on If"))
+        )
+    | Id n -> lookup n env
+    | Let (s,e,b) -> eval ((s,eval env e)::env) b
+    (*
+    | LetRec (s,e,b) ->
+    *)
+    | App (e1,e2) -> (match eval env e1, eval env e2 with
+        | Closure (s,e,env), v -> eval ((s,v)::env) e
+        | x,_ -> x
+        )
+    | Lambda (s,e) -> Closure (s,e,env)
 
+
+
+ let evaluate (e:expr) : value = eval [] e
+
+
+
+ (* Some sample expressions *)
 let inc = Lambda ("n", Add(Id "n", Value (Int 1)))
 
 let add = Lambda ("x",
                   Lambda ("y", Add (Id "x", Id "y"))
                  )
 
-(* The 'sumToN' function *)
+ (* The 'sumToN' function *)
 let sumToN_expr : expr =
-    LetRec ("sumToN", 
-            Lambda ("n", 
+    LetRec ("sumToN",
+            Lambda ("n",
                     If (Eq (Id "n", Value (Int 0)),
                         Value (Int 0),
-                        Add (Id "n", 
-                             App (Id "sumToN", 
+                        Add (Id "n",
+                             App (Id "sumToN",
                                   Sub (Id "n", Value (Int 1))
                                  )
                             )
@@ -58,5 +140,6 @@ let sumToN_expr : expr =
             Id "sumToN"
            )
 
-
+(*
 let twenty_one : value = evaluate (App (sumToN_expr, Value (Int 6)));
+*)
--- Hwk_04/tests.ml
+++ Hwk_04/tests.ml
@@ -1,4 +1,5 @@
 #use "arithmetic.ml" ;;
+#use "eval.ml" ;;
 
 
 
@@ -9,7 +10,6 @@ print_string "Tests for show_expr passed." ;;
 
 
 
-
 assert (show_pretty_expr (Add (Const 1, Mul (Const 3, Const 4))) = "1+3*4") ;;
 assert (show_pretty_expr (Add (Mul (Const 1, Const 3), Const 4)) = "1*3+4") ;;
 assert (show_pretty_expr (Add (Const 1, Add (Const 3, Const 4))) = "1+(3+4)") ;;
@@ -17,3 +17,26 @@ assert (show_pretty_expr (Add (Add (Const 1, Const 3), Const 4)) = "1+3+4") ;;
 assert (show_pretty_expr (Mul (Const 4, Add (Const 3, Const 2))) = "4*(3+2)") ;;
 assert (show_pretty_expr (Sub (Sub (Const 1, Const 2), Sub (Const 3, Const 4))) = "1-2-(3-4)") ;;
 print_string "Tests for show_pretty_expr passed." ;;
+
+
+
+assert (freevars (Add (Value (Int 3), Mul (Id "x", Id "y"))) = ["x"; "y"]) ;;
+assert (freevars (Let ("x", Id "z", Add (Value (Int 3), Mul (Id "x", Id "y")))) = ["z"; "y"] ) ;;
+assert (freevars (Let ("x", Id "x", Add (Value (Int 3), Mul (Id "x", Id "y")))) = ["x"; "y"] ) ;;
+assert (freevars (Lambda ("x", Add (Value (Int 3), Mul (Id "x", Id "y")))) = ["y"] ) ;;
+assert (freevars sumToN_expr = [] ) ;;
+print_string "Tests for freevars passed." ;;
+
+
+
+assert (evaluate (Add (Value (Int 1), Mul (Value (Int 2), Value (Int 3)))) = Int 7) ;;
+assert (evaluate (Eq (Value (Int 1), Mul (Value (Int 2), Value (Int 3)))) = Bool false) ;;
+assert (evaluate (Lt (Value (Int 1), Mul (Value (Int 2), Value (Int 3)))) = Bool true) ;;
+assert (evaluate (If (Lt (Value (Int 1), Mul (Value (Int 2), Value (Int 3))),
+            Value (Int 4),
+            Value (Int 5))) = Int 4) ;;
+assert (evaluate inc = Closure ("n", Add (Id "n", Value (Int 1)), []) ) ;;
+assert (evaluate add = Closure ("x", Lambda ("y", Add (Id "x", Id "y")), []) ) ;;
+assert (evaluate (App (add, Value (Int 1))) = Closure ("y", Add (Id "x", Id "y"), [("x", Int 1)]) ) ;;
+assert (evaluate (App ( (App (add, Value (Int 1))), Value (Int 2))) = Int 3 ) ;;
+print_string "Tests for evaluate passed." ;;
